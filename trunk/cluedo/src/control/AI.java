30f597d19cbbd0cc1d6dad690225e567b840ae38
barbara.sepic@gmail.com <barbara.sepic@gmail.com@34b8e0fd-77f5-1f13-f2b2-e3511a880627>
2011-12-18 14:06:59 +0000

    UML to Java, initial class proposal

---
+++ trunk/cluedo/src/control/AI.java
@@ -0,0 +1,32 @@
+package control;
+
+import java.util.List;
+
+public class AI extends Player{
+
+	@Override
+	public Suggestion play() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Card receiveSuggestion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void receiveAnswer(Card card, Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void observeMove(Suggestion suggestion, List<Player> couldNotAnswer,
+			Player answered) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ trunk/cluedo/src/control/Kind.java
@@ -0,0 +1,7 @@
+package control;
+
+public enum Kind {
+	person,
+	room,
+	weapon,
+}
