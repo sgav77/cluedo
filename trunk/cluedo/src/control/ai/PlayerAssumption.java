d6ecb1c58d45af5ca801aa0948f573631c358790
fstahlberg@gmail.com <fstahlberg@gmail.com@34b8e0fd-77f5-1f13-f2b2-e3511a880627>
2011-12-25 07:19:00 +0000

    - Set up Observer relationships - Introduce SearchSpace - Add basic logic to PlayerAssumption - Notify all participants in control.ai package about players hand cards

--- trunk/cluedo/src/control/ai/PlayerAssumption.java
+++ trunk/cluedo/src/control/ai/PlayerAssumption.java
@@ -1,12 +1,28 @@
 package control.ai;
 
 import java.util.HashSet;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Set;
 
 import control.Card;
 import control.Player;
 
-public class PlayerAssumption {
+/**
+ * This class maintains the knowledge about hand cards of other players. It
+ * stores a list of certain and possible hand cards and notifies its observers
+ * whenever a new hand card is certainly derived. Observers are other instances
+ * of PlayerAssumption or the SearchSpace. The methods removePossibleCard() and
+ * addCertainCard() contain some simple hard-coded logic for direct conclusions
+ * (for example if all hand cards are known, there are no possible hand cards
+ * anymore).
+ * 
+ * @see control.Player
+ * @see #removePossibleCard(Card)
+ * @see #addCertainHandCard(Card)
+ * @see java.util.Observable
+ */
+public class PlayerAssumption extends Observable implements Observer {
 	
 	private Player player;
 	private Set<Card> possibleHandCards;
@@ -30,7 +46,9 @@ public class PlayerAssumption {
 	
 	/**
 	 * Remove a card from the set of possible cards. If this set does not
-	 * contain this card, do nothing.
+	 * contain this card, do nothing. If after adding this card we have as 
+	 * much possible cards as free player hand card slots, remove all possible
+	 * hand cards and add the remaining to cetrainHandCards.
 	 * 
 	 * @param card card to remove
 	 * @throws NullPointerException if card is null
@@ -40,6 +58,17 @@ public class PlayerAssumption {
 			throw new NullPointerException();
 		}
 		this.possibleHandCards.remove(card);
+		if (this.certainHandCards.size() + this.possibleHandCards.size() 
+				== this.player.countHandCards()) {
+			this.certainHandCards.addAll(possibleHandCards);
+			possibleHandCards.clear();
+			// Notify about more cards than necessary, but otherwise we have
+			// conflicts with the removal mechanism in addCertainHandCard()
+			for (Card certainCard : certainHandCards) {
+				this.setChanged();
+				this.notifyObservers(certainCard);
+			}
+		}
 	}
 	
 	/**
@@ -50,16 +79,49 @@ public class PlayerAssumption {
 	 * @param card
 	 * @throws NullPointerException if card is null
 	 */
-	public void addCertainCard(Card card) throws NullPointerException {
+	public void addCertainHandCard(Card card) throws NullPointerException {
 		if (card == null) {
 			throw new NullPointerException();
 		}
-		assert this.possibleHandCards.contains(card);
+		if (!this.possibleHandCards.contains(card)) {
+			return; // Already added / not possible
+		}
+		this.certainHandCards.add(card);
 		this.removePossibleCard(card);
-		this.certainHandCards.remove(card);
+		this.setChanged();
+		this.notifyObservers(card);
 		if (this.certainHandCards.size() == this.player.countHandCards()) {
 			this.possibleHandCards.clear();
 		}
+	}
+	
+	/**
+	 * This method is called whenever a certain card is added somewhere else.
+	 * We can then exclude this card from our possible cards pool.
+	 * 
+	 * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
+	 * @throws NullPointerException if arg is NULL
+	 * @throws IllegalArgumentException if arg is not a Card
+	 */
+	@Override
+	public void update(Observable o, Object arg) {
+		if (arg == null) {
+			throw new NullPointerException();
+		}
+		if (!(arg instanceof Card)) {
+			throw new IllegalArgumentException();
+		}
+		this.removePossibleCard((Card) arg);
+	}
+
+	/**
+	 * Returns if this player is fully explored, meaning that we know each of
+	 * his/her hand card.
+	 * 
+	 * @return TRUE if we know all hand cards, FALSE otherwise
+	 */
+	public boolean isFullyExplored() {
+		return possibleHandCards.isEmpty();
 	}
 
 	/**
